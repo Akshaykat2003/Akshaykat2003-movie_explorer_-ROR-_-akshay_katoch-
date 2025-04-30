fd75a9ef893395b0e9c1aeb79c5f0af8866b487b
akshay <akshaykatoch38@gmail.com>
2025-04-30 13:01:50 +0530

    [Akshay] MOD:Fix the cors issue for frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/api/*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true 
   end
 end
