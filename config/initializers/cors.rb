f03333bba5bf42da35acd737b9317445b51e7a83
akshay <akshaykatoch38@gmail.com>
2025-05-02 18:23:39 +0530

    [Akshay] MOD:Updated cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://movieexplorerplus.netlify.app/'
-
+    origins 'http://localhost:5173', 'https://movieexplorerplus.netlify.app'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      expose: ['Authorization']
   end
 end
