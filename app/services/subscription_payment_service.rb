409f46497921a221c205260d10a9a518f49a99da
akshay <akshaykatoch38@gmail.com>
2025-05-16 16:36:57 +0530

    [akshay] MOD:Increase the time for Gold expiry from 1 minute to 2 minute

--- app/services/subscription_payment_service.rb
+++ app/services/subscription_payment_service.rb
@@ -23,12 +23,11 @@ class SubscriptionPaymentService
     currency = price.currency 
 
     expiry_date = case plan
-                  when 'gold' then Time.current + 1.minute
+                  when 'gold' then Time.current + 2.minute
                   when 'platinum' then Time.current + 1.month
                   end
 
     if is_mobile
-   
       payment_intent = Stripe::PaymentIntent.create(
         customer: customer.id,
         amount: amount,
